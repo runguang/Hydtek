b3722048113de189e12da68798b74cfd85713ea5
runguang <runguang@live.com.sg>
2016-05-10 10:28:27 -0400

    Updates

--- HydtekWeb/HydtekWeb/urls.py
+++ HydtekWeb/HydtekWeb/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from hello import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # try stuff
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
